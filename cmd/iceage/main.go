d97b129cd23f4395c3b27d6bc0c6d644a4644abe
rho <ryan@theoleschool.com>
2023-01-26 20:38:31 -0600

    First commit of forecast getter and basic formatter.

--- cmd/iceage/main.go
+++ cmd/iceage/main.go
@@ -22,17 +22,33 @@ func main() {
 	fmt.Println(mastoURL)
 	fmt.Println(nwsURL)
 
+	w := nws.NewClient()
+
 	// Get Weather Alerts!
 	alerts := nws.AlertList{}
-	w := nws.NewClient()
 	alerts, err = w.FetchAlerts()
 	if err != nil {
 		log.Fatal("There was a problem fetching alerts from NWS: ", err)
 	}
 
-	//Try to toot!
+    // Get forecast!
+    forecast := nws.Forecast{}
+    forecast, err = w.FetchForecast()
+    if err != nil {
+        log.Fatal("There was a problem fetching the forecast from NWS: ", err)
+    }
+
+
 	t := toot.NewClient(os.Getenv("ACCESS_TOKEN"))
+
+	// Try to toot the alert!
 	if err = t.PostToot(alerts.Features[0].Properties.Event, alerts.Features[0].Properties.Description); err != nil {
 		log.Fatal("There was a problem tooting: ", err)
 	}
+
+    // Tey to toot the forecast!
+    formattedForecast := w.FormatForecast(forecast)
+    if err = t.PostToot("2 day Forecast", formattedForecast); err != nil {
+        log.Fatal("There was a problem tooting: ", err)
+    }
 }
---
+++ pkg/nws/forecast.go
@@ -0,0 +1,46 @@
+package nws
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Forecast struct {
+	Properties ForecastDetails `json:"properties"`
+}
+
+type ForecastDetails struct {
+	Updated string           `json:"updated"`
+	Periods []ForecastPeriod `json:"periods"`
+}
+
+type ForecastPeriod struct {
+	Name             string `json:"name"`
+	DetailedForecast string `json:"detailedForecast"`
+}
+
+func (c *Client) FetchForecast() (Forecast, error) {
+    var res Forecast
+    getURL := fmt.Sprintf("%s/zones/forecast/%s/forecast", c.BaseURL, c.CountyCode)
+
+    req, err := http.NewRequest("GET", getURL, nil)
+    if err != nil {
+        return res, err
+    }
+
+    if err = c.sendRequest(req, &res); err != nil {
+        return res, err
+    }
+
+    return res, nil
+}
+
+func (c *Client) FormatForecast(forecastResponce Forecast) string {
+    var forecastText string
+
+    for d:=0; d<4; d++ {
+        forecastText += fmt.Sprintf("%s\n%s\n", forecastResponce.Properties.Periods[d].Name, forecastResponce.Properties.Periods[d].DetailedForecast)
+    }
+
+    return forecastText
+}
